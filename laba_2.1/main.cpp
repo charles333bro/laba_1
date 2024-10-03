b7c0ccfddbb7340a511afbe9440fe3ead1c77a7b
charles333bro <162841385+charles333bro@users.noreply.github.com>
2024-10-03 22:50:22 +0200

    добавлена лаба2
    да-да

---
+++ laba_2.1/main.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <cmath> // для функции sin и M_PI
+
+int main() {
+    double A, B, angle_degrees, angle_radians, area;
+
+    // Ввод данных
+    std::cout << "Введите длину стороны A: ";
+    std::cin >> A;
+    std::cout << "Введите длину стороны B: ";
+    std::cin >> B;
+    std::cout << "Введите угол a (в градусах): ";
+    std::cin >> angle_degrees;
+
+    // Преобразование угла в радианы
+    angle_radians = angle_degrees * 3.1415926535 / 180.0;
+
+    // Расчет площади
+    area = 0.5 * A * B * sin(angle_radians);
+
+    // Вывод результата
+    std::cout << "Площадь треугольника: " << area << std::endl;
+
+    return 0;
+}
